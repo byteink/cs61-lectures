f6f6599eb9e612e82f532a8e75b4ffad683504c2
Mike Smith <mike_smith@harvard.edu>
2019-09-24 11:06:27 -0400

    Lecture 7 code.

---
+++ asm1/f07x.cc
@@ -0,0 +1,14 @@
+//! -O1
+
+int ai = 10;
+unsigned au = 10;
+int aa[2] = {10, 20};
+int bi = -1;
+
+int sum() {
+    return ai + au + aa[0] + bi;
+}
+
+main () {
+    return sum();
+}
---
+++ asm1/f07x.s
@@ -0,0 +1,53 @@
+	.file	"f07x.cc"
+	.text
+	.globl	_Z3sumv
+	.type	_Z3sumv, @function
+_Z3sumv:
+.LFB0:
+	.cfi_startproc
+	movl	aa(%rip), %eax
+	addl	ai(%rip), %eax
+	addl	au(%rip), %eax
+	addl	bi(%rip), %eax
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	_Z3sumv, .-_Z3sumv
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	call	_Z3sumv
+	rep ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.globl	bi
+	.data
+	.align 4
+	.type	bi, @object
+	.size	bi, 4
+bi:
+	.long	-1
+	.globl	aa
+	.align 8
+	.type	aa, @object
+	.size	aa, 8
+aa:
+	.long	10
+	.long	20
+	.globl	au
+	.align 4
+	.type	au, @object
+	.size	au, 4
+au:
+	.long	10
+	.globl	ai
+	.align 4
+	.type	ai, @object
+	.size	ai, 4
+ai:
+	.long	10
+	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
+	.section	.note.GNU-stack,"",@progbits
